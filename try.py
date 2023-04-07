953494ce31bc8fdc8c99af004f40b982ab1a949a
OthmanAlkhatib <97898775+OthmanAlkhatib@users.noreply.github.com>
2023-04-08 01:36:01 +0300

    Update try.py

--- try.py
+++ try.py
@@ -10,3 +10,5 @@ print("another eidt in main branch from local after edit from github")
 print("edit in main branch from github part 2")
 print("hellllllloooo from looooocal")
 print("edit in main branch from github 2")
+
+print("edit from github before merging the content of try branch")
